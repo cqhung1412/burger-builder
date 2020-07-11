f73870c1b92ab56427ac8ebfd438da3108252d42
Nati-onBear <42210002+Nati-onBear@users.noreply.github.com>
2020-07-12 02:17:52 +0700

    Add BurgerBuilder Container
    Edit webpack.config for css modules

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import logo from './assets/logo.svg';
-import './App.css';
+import classes from './App.css';
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={classes.App}>
+      <header className={classes.AppHeader}>
+        <img src={logo} className={classes.AppLogo} alt="logo" />
         <p>Bear Burger Builder</p>
       </header>
       <Layout>
-        <h3>Test</h3>
+        <BurgerBuilder />
       </Layout>
     </div>
   );
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import classes from './Layout.css'
 import Auxiliary from '../../hoc/Auxiliary'
 
 const Layout = (props) => {
   return (
     <Auxiliary>
       <div>Toolbar, SideDrawer, Backdrop</div>
-      <main>
+      <main className={classes.Content}>
         {props.children}
       </main> 
     </Auxiliary>
