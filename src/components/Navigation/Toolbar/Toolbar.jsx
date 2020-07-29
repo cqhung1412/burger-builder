e5a4138f51552fb87bad5a7be1c5ee305d2f5800
Nati-onBear <42210002+Nati-onBear@users.noreply.github.com>
2020-07-30 06:49:59 +0700

    Adding Basic Nav

--- src/components/Navigation/Toolbar/Toolbar.jsx
+++ src/components/Navigation/Toolbar/Toolbar.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import logo from '../../../assets/images/burger-logo.png';
 import classes from './Toolbar.css'
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = () => {
   return (
     <header className={classes.ToolbarHeader}>
       <img src={logo} className={classes.ToolbarLogo} alt="logo" />
+      <nav>
+        <NavigationItems />
+      </nav>
       <p>Bear Burger Builder</p>
     </header>
   )
